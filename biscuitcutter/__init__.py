efc8411eaa4baf42aa0bb74ec99b448b65a577ec
erdogant <erdogant@gmail.com>
2026-08-29 16:33:36 +0200

    1.1.4

--- biscuitcutter/__init__.py
+++ biscuitcutter/__init__.py
@@ -5,7 +5,7 @@ from biscuitcutter.biscuitcutter import main
 
 __author__ = 'Erdogan Taskesen'
 __email__ = 'erdogant@gmail.com'
-__version__ = '1.1.3'
+__version__ = '1.1.4'
 
 # Setup package-level logger
 _logger = logging.getLogger('biscuitcutter')
